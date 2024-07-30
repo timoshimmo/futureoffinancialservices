db32b1dab87dc51a8039e99814d7628230072f74
Tokmang Wang <timoshimmo21@gmail.com>
2024-07-30 10:22:04 +0100

    updated changes

--- src/pages/Landing/Home/CTA.tsx
+++ src/pages/Landing/Home/CTA.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const CTA = () => {
     return (
         <React.Fragment>
-            <section className="py-5 position-absolute bg-white border rounded-5 web-cta-home">
+            <section className="py-5 mx-auto position-absolute bg-white border rounded-5 web-cta-home">
                 <div className="bg-overlay bg-overlay-pattern opacity-50"></div>
                 <Container>
                     <Row className="justify-content-center align-items-center gy-4">
@@ -17,7 +17,7 @@ const CTA = () => {
                         </Col>
                         <Col lg={8}>
                             <div className="mt-3 hstack p-2 justify-content-between border border-1 rounded-5 cta-form">
-                                <input type="email" placeholder="example@email.com" className="border-0 fs-15 px-3"  style={{ color: '#141413', backgroundColor: 'transparent' }} />
+                                <input type="email" placeholder="example@email.com" className="border-0 fs-15 px-3 w-100"  style={{ color: '#141413', backgroundColor: 'transparent' }} />
 								<input type="button" className="btn btn-primary border border-primary rounded-5 py-2 fs-16" value="Subscribe" style={{ fontFamily: 'Georgia, "Times New Roman", Times, serif' }}/>
                             </div>
                         </Col>
