95b1e962ff32a77b7fcec15195bf57258de6a4d5
Tokmang Wang <timoshimmo21@gmail.com>
2024-07-30 18:41:07 +0100

    Update UI

--- src/pages/Landing/Home/Speakers.tsx
+++ src/pages/Landing/Home/Speakers.tsx
@@ -62,14 +62,6 @@ const Speakers = () => {
                     </div>
     </Row>*/}
                 <Row className='web-featured-speakers'>
-                <div ref={sliderRef} className="keen-slider">
-                        <div className="keen-slider__slide number-slide1">1</div>
-                        <div className="keen-slider__slide number-slide2">2</div>
-                        <div className="keen-slider__slide number-slide3">3</div>
-                        <div className="keen-slider__slide number-slide4">4</div>
-                        <div className="keen-slider__slide number-slide5">5</div>
-                        <div className="keen-slider__slide number-slide6">6</div>
-                    </div>
                     <Marquee
                         pauseOnClick={true}>
                             {speakersData.map((item, key) => (
