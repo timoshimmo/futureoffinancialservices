89460ac8095777cab45a2f1e681cc7e7e6e1598f
Tokmang Wang <timoshimmo21@gmail.com>
2024-08-02 00:19:26 +0100

    Update UI

--- src/pages/Landing/Home/Connect.tsx
+++ src/pages/Landing/Home/Connect.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef  } from 'react';
+import React, { useState, useRef, RefObject  } from 'react';
 import { Container, Row, Col, Card, CardBody } from 'reactstrap';
 //import { Link } from 'react-router-dom';
 import { connectData } from '../../../common/data';
@@ -10,27 +10,30 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
+import 'swiper/css/scrollbar';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Scrollbar, Pagination, Navigation } from 'swiper/modules';
 
 const Connect = () => {
 
     const [imgName, setImageName] = useState(summit1);
+
+    const [mobileImg, setMobileImg] = useState(summit1);
     const [indexVal, setIndexVal] = useState(0);
-    const swiperRef = useRef<HTMLInputElement | null>(null);
+    //const swiperRef = useRef<RefObject | null>(null);
 
 
-    swiperRef.current?.addEventListener('swiperslidechange', (e) => {
-        //console.log(e.detail);
-    });
+   /* swiperRef.current?.addEventListener('swiperslidechange', (e) => {
+        console.log(e.currentTarget);
+    }); */
     
     return (
         <React.Fragment>
             <section className="submit-section body-bg-dark one-summit-section" style={{ backgroundColor: "#141413" }}>
                 <Container>
                     {/* summit-padding web-one-summit */}
-                    <Row className="gy-4">
+                    <Row className="gy-4 web-one-summit">
                         <Col lg={12} className='summit-padding'>
                             <div className="w-100">
                                 <h2 className="mb-4 fw-semibold lh-base text-secondary" style={{ fontFamily: 'Georgia, "Times New Roman", Times, serif' }}>One Summit, <br /><span className='text-primary'>Unlimited Possibilities</span></h2>
@@ -56,17 +59,12 @@ const Connect = () => {
                             </div>
                         </Col>
                     </Row>
-                    {/*<Row className="mobile-one-summit justify-content-center">
+                    <Row className="mobile-one-summit justify-content-center position-relative">
+                        <div className='swiper-scrollbar position-relative bg-white mb-4' style={{ height: '10px' }}></div>
                         <Col sm={12}>
-                            <div className="w-100 mb-4 d-flex justify-content-center">
-                                <span className='swiper-pagination'></span>
-                                <span className='swiper-pagination'></span>
-                                <span className='swiper-pagination'></span>
-                                <span className='swiper-pagination'></span>
-                            </div>
                             <Swiper
                                 spaceBetween={20}
-                                centeredSlides={true}
+                                centeredSlides={false}
                                 pagination={{
                                     el: '.swiper-pagination',
                                     clickable: true,
@@ -74,11 +72,15 @@ const Connect = () => {
                                     bulletClass: "bg-amber-400",
                                     bulletActiveClass: "bg-primary"
                                 }}
-                                slidesPerView={1.5}
+                                scrollbar= {{
+                                    el: '.swiper-scrollbar',
+                                    draggable: true,
+                                  }}
+                                slidesPerView={1}
                                 navigation={false}
-                                modules={[Pagination, Navigation]}
-                                className="my-swiper"
-                               
+                                modules={[Pagination, Navigation, Scrollbar]}
+                                className="mySwiper"
+                                onSlideChange={(swiper) => setMobileImg(connectData[swiper.realIndex].img) /*console.log(connectData[swiper.realIndex].img)*/}
                             >
                                 {connectData.map((item, key) => (
                                     <SwiperSlide key={key} className='mb-5'>
@@ -97,7 +99,12 @@ const Connect = () => {
                                 ))}
                             </Swiper>
                         </Col>
-                    </Row> */}
+                        <Col lg={5} sm={12} className="mx-auto">
+                            <div className='sponsor-left'>
+                                <img src={mobileImg} alt="" className="img-fluid mx-auto border border-0 rounded-4" />
+                            </div>
+                        </Col>
+                    </Row> 
                 </Container>
             </section>
         </React.Fragment>
